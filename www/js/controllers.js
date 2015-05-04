68d44c3b8eca78e6469dc06ad7364ea8dc1ba55c
Dane Burkland <dane.a.burkland@gmail.com>
2015-05-04 10:46:59 -0500

    initial

---
+++ www/js/controllers.js
@@ -0,0 +1,68 @@
+angular.module('bearTracks.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('CalendarCtrl', function($scope) {
+    /* config object */
+    $scope.eventSources = [];
+    $scope.uiConfig = {
+      calendar:{
+        height: 450,
+        editable: true, 
+        header:{
+          left: '',
+          center: 'title',
+          right: 'prev,next'
+        },
+        dayClick: $scope.alertEventOnClick,
+        eventDrop: $scope.alertOnDrop,
+        eventResize: $scope.alertOnResize
+      }
+    };
+    $scope.entry = {};
+})
+
+.controller('CategoriesCtrl', function($scope) {
+  $scope.categories = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('CategoryCtrl', function($scope, $stateParams) {
+});
---
+++ www/js/directives.js
@@ -0,0 +1,3 @@
+angular.module('bearTracks.controllers', [])
+
+  .directive('')
